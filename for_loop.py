0b25c85e17ef9aa410a4c34c5bcd2e4d64324c7a
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-03-01 00:50:57 +0600

    first commit

---
+++ for_loop.py
@@ -0,0 +1,20 @@
+'''for count in range(1,12,2):
+    print(count)'''
+'''10
+9
+8
+7
+.
+.
+.
+1
+'''
+'''for count in range(10,0,-1):
+    print(count)'''
+
+#1 2 3 4 5 6
+sum = 0
+for i in range(1,7):
+    sum = sum + i
+
+print(sum)
